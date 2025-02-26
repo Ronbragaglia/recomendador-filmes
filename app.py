11ee6750813309bdde994a05cb97d3f111d16d06
Rone Bragaglia <122837980+Ronbragaglia@users.noreply.github.com>
2025-02-26 17:22:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# Aqui você coloca o código atualizado do app.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "API funcionando corretamente!"}
+
+git add app.py
+git commit -m "Corrigindo erro de ASGI no FastAPI"
+git push origin main
